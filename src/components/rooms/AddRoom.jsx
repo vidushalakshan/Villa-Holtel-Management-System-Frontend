01630aed9af4b17114112478d7539e4acdddf757
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-05 00:06:15 +0530

    updated AddRoom some content

--- src/components/rooms/AddRoom.jsx
+++ src/components/rooms/AddRoom.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const AddRoom = () => {
+    const [newRoom, setNewRoom] = useState({
+        photo : null,
+        roomType: "",
+        roomPrice: ""
+    })
+
+    const[imagePreview, setImagePreview] = useState("")
+    const[successMessage, setSuccessMessage] = useState("")
+    const[errorMessage, setErrorMessage] = useState("")
+
+    const handleRoomInputChange = (e) => {
+        const name = e.target.name
+        let value = e.target.value
+        if(name === "roomPrice") {
+            if(!isNaN(value)){
+                value.parseInt(value)
+            }else {
+                value=""
+            }
+        }
+        setNewRoom({...newRoom, [name]: value})
+    } 
+
   return (
     <div>
       
