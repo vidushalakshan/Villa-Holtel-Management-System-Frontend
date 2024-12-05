a6726f08adfceba6bca416af80279ae3b512a3ae
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-05 18:54:23 +0530

    updated handleonSubmit content in addroom

--- src/components/rooms/AddRoom.jsx
+++ src/components/rooms/AddRoom.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { addRoom } from '../utils/ApiFunction'
 
 const AddRoom = () => {
     const [newRoom, setNewRoom] = useState({
@@ -23,6 +24,27 @@ const AddRoom = () => {
         }
         setNewRoom({...newRoom, [name]: value})
     } 
+
+    const hendleImageChange = (e) => {
+        const selectedImage = e.target.files[0]
+        setNewRoom({...newRoom, phooto: selectedImage})
+        setImagePreview(URL.createObjectURL(selectedImage))
+    }
+
+    const handleSubmit  = async (e) => {
+        e.preventDefault()
+        try{
+            const success = await AddRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
+            if(success !== underfined) {
+                setSuccessMessage("A new room added to the database")
+                setNewRoom({photo: null, roomType:"", roomPrice:""})
+                setImagePreview("")
+                setErrorMessage("")
+            }
+        }catch(error){
+            setErrorMessage(error.message)
+        }
+    }
 
   return (
     <div>
