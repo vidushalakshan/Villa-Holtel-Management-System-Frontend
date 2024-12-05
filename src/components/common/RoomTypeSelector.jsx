b0528b081cd4fc9050ed49a45acf0bf0eeb1cb11
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-05 22:30:22 +0530

    updated RoomTypeSelector content

--- src/components/common/RoomTypeSelector.jsx
+++ src/components/common/RoomTypeSelector.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getRoomTypes } from '../utils/ApiFunction'
 
 const RoomTypeSelector = () => {
+    const [roomTypes , setRoomTypes] = useState(false)
+    const [showNewROomTypeInput , setShowNewRoomTypesInput] = useState(false)
+    const [newRoomType, setNewRoomType] = useState("")
+
+    useEffect(() => {
+        getRoomTypes().then((data) => {
+            setRoomTypes(data)
+        })
+    }, [])
+    
+    const handleNewRoomInputChange = (e) => {
+        setNewRoomType(e.target.value);
+    }
+
   return (
     <div>
-      
+        
     </div>
   )
 }
