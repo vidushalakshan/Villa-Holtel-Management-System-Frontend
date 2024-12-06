4eddfb23d5993fb23ac9725619d6511b394c2fd9
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-06 22:14:19 +0530

    updated RoomTypeSelector contents

--- src/components/common/RoomTypeSelector.jsx
+++ src/components/common/RoomTypeSelector.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { getRoomTypes } from '../utils/ApiFunction'
 
-const RoomTypeSelector = () => {
-    const [roomTypes , setRoomTypes] = useState(false)
+const RoomTypeSelector = ({handleRoomInputChange, newRoom}) => {
+    const [roomTypes , setRoomTypes] = useState([""])
     const [showNewROomTypeInput , setShowNewRoomTypesInput] = useState(false)
     const [newRoomType, setNewRoomType] = useState("")
 
@@ -12,14 +12,58 @@ const RoomTypeSelector = () => {
         })
     }, [])
     
-    const handleNewRoomInputChange = (e) => {
+    const handleNewRoomTypeInputChange = (e) => {
         setNewRoomType(e.target.value);
     }
 
+    const handleAddNewRoomType = () => {
+        if(newRoomType !== "") {
+            setRoomTypes([...roomTypes, newRoomType])
+            setNewRoomType("")
+            setShowNewRoomTypesInput(false)
+        }
+    }
+
   return (
-    <div>
-        
-    </div>
+    <>
+        {roomTypes.length > 0 && (
+            <div>
+                <select
+                id='roomType'
+                name='roomType'
+                value={newRoom.roomTypes}
+                onChange={(e) => {
+                    if(e.target.value === "Add New"){
+                        setShowNewRoomTypesInput(true)
+                    }else{
+                        handleRoomInputChange(e)
+                    }
+                }}
+                >
+                    <option value={""} >select a room type</option>
+                    <option value={"Add New"}>Add New</option>
+                    {roomTypes.map((type, index) => (
+                        <option key={index} value={type}>
+                            {type}
+                        </option>
+                    ))}
+                </select>
+                {showNewROomTypeInput && (
+                    <div className='input-group'>
+                        <input 
+                        type="text" 
+                        className='form-control'
+                        placeholder='Enter a new room type'
+                        onChange={handleNewRoomTypeInputChange}
+                        />
+                        <button className='btn btn-hotel' type='button' onClick={handleAddNewRoomType}>
+                            Add
+                        </button>
+                    </div>
+                )}
+            </div>
+        )}
+    </>
   )
 }
 
