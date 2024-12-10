985b4f47223e2cb30832c1c8a832f9d7bcf7c5c0
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-10 20:27:51 +0530

    updated roomPaginator

--- src/components/common/RoomPaginator.jsx
+++ src/components/common/RoomPaginator.jsx
@@ -5,7 +5,16 @@ const RoomPaginator = ({currentPage, totalPages, onPageChange}) => {
   return (
     <nav>
         <ul className='pagination, justify-content-center '>
-
+          {pageNumbers.map((pageNumber) => (
+            <li 
+              key={pageNumber}
+              className={`page-item ${currentPage === pageNumber ? "active" : ""}`}
+            >
+              <button className='page-link' onClick={() => onPageChange(pageNumber)}>
+                {pageNumber}
+              </button>
+            </li>
+          ))}
         </ul>
     </nav>
   )
